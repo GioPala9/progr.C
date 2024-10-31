fe903a727b48e86f03ad9867fd9c7f0cc9755c90
GioPala9 <giovanniclaudio.pala@gmail.com>
2024-10-31 13:28:53 +0000

    primo codice scritto

---
+++ lezione 31/10/24/MAIN.c
@@ -0,0 +1,17 @@
+/*
+* AUTORE: GIO.PALA
+* DATA: 31/10/25 (halloween)
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#define pig 3.14
+
+float diametro=3;
+float circ;
+
+int main (int argc, char *argv[]) {
+    circ=diametro*pig;
+    printf("la circonferenza vale %f cm\n", circ);
+    return 0;
+}
+  
