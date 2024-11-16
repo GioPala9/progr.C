e0f2343867ab7f9490eef95722fb848fa00475ee
GioPala9 <giovanniclaudio.pala@gmail.com>
2024-11-16 08:00:11 +0000

    ghk

---
+++ lezione 16/11/24/MAIN1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    char pippo;
+    printf("ciao bomber! \ncome ti chiami?");
+    scanf("pippo");
+    printf("bel nome dimmerda !!");
+    return 0;
+
+}
