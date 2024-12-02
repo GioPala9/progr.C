35b1ddb13e1910a50b176e09a511b54cb5329863
GioPala9 <giovanniclaudio.pala@gmail.com>
2024-12-02 11:00:13 +0000

    45y67rth

---
+++ lezione 2/12/24/MAIN1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+    float pippo, gigi;
+    printf("inserisci un numero");
+    scanf ("%f");
+    {
+    if (pippo >= gigi)
+    printf("il numero %f è maggiore o uguale di %f", pippo, gigi);
+    }
+    {
+    else
+    printf("il numero %f è maggiore o uguale di %f", gigi, pippo);
+        
+    }
+    
+}
