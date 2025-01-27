9f38d5d0ba252a3e0d16d1a4014b4ee25f705a97
GioPala9 <giovanniclaudio.pala@gmail.com>
2025-01-27 10:59:21 +0000

    erghz

---
+++ lezione 27/01/MAIN.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    char pierino [10]={'a','b','c','d','e','f','g','h','i','l'};
+
+    printf("la lettera scelta:%c", pierino[2]);
+    return 0;
+}
