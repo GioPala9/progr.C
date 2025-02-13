bc86b819b14b859ed8e6c48b9873a3a398b26385
GioPala9 <giovanniclaudio.pala@gmail.com>
2025-02-13 13:22:16 +0000

    commit2

--- lezione 13/02/MAIN1.c
+++ lezione 13/02/MAIN1.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main () 
+int main ()
 {
     FILE *gigio_ptr;
 
-    gigio_ptr = fopen("ziocane.txt", "w");
+    gigio_ptr = fopen("c:\\ziocane.txt", "w");
 
+    fprintf(gigio_ptr, "Ciao\n");
     fclose(gigio_ptr);
+
     
+
+
 }
